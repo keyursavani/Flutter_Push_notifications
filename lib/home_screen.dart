144bdb4486868c620ca75eff585f21bdde194747
keyur <savanikeyur1011@gmail.com>
2023-08-17 09:03:09 +0530

    final commit

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,6 +1,10 @@
+import 'dart:convert';
+
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:push_notification_tutorial/notification_services.dart';
+import 'package:http/http.dart' as http;
 
 class HomeScreen extends StatefulWidget{
   @override
@@ -18,10 +22,12 @@ class HomeScreen extends StatefulWidget{
     notificationServices.requestNotificationPermission();
     notificationServices.firebaseInit(context);
     notificationServices.setupInteractMessage(context);
-    // notificationServices.isTokenRefresh();
+    notificationServices.isTokenRefresh();
     notificationServices.getDeviceToken().then((value) {
-      print("Device Token");
-      print(value);
+     if(kDebugMode){
+       print("Device Token");
+       print(value);
+     }
     });
   }
   @override
@@ -31,7 +37,34 @@ class HomeScreen extends StatefulWidget{
       appBar: AppBar(
         title: Text("Home Screen"),
       ),
-      body: Container(),
+      body: Center(
+        child: TextButton(
+          onPressed: (){
+            notificationServices.getDeviceToken().then((value) async{
+              var data = {
+                'to': value.toString(),
+                'priority':'high',
+                'notification':{
+                  'title' : 'Title',
+                  'body': 'Body'
+                },
+                'data':{
+                  'type':'msj',
+                  'id':'123456'
+                },
+              };
+              await http.post(Uri.parse('https://fcm.googleapis.com/fcm/send'),
+              body: jsonEncode(data),
+                headers: {
+                  'Content-Type' :'application/json; charset=UTF-8',
+                  'Authorization' : 'key=AAAAJpSawxU:APA91bEgd7WkzJCmxCNBlVWrlUEV1LOvoHB0sG-WbCgQk9yXnB6ofpN9eHmkV_xlMxKOBkSlaWDXTQEWQ3Zdx8361Kfc3fcJsd7tTQhHcIAGw_wI27doeY4jonK2wZDUp7spA9C8HVTe'
+                }
+              );
+            });
+          },
+          child:Text("Send Message"),
+        ),
+      ),
     );
   }
  }
